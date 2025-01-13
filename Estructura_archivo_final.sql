cd9c9a15d97e4e969027b35a9059a3d6a44def75
martinlopez7 <144948913+martinlopez7@users.noreply.github.com>
2025-01-13 14:23:57 +0100

    correcciones

--- Estructura_archivo_final.sql
+++ Estructura_archivo_final.sql
@@ -1,61 +1,69 @@
 -- Tabla e inserts de (persona 1)
 tabla 1
 inserts tabla 1
+
 -- Tabla e inserts de (persona 2)
 tabla 2
 inserts tabla 2
+
 -- Tabla e inserts de (persona 3)
 tabla 3
 inserts tabla 3
+
 -- Tabla e inserts de (persona 4)
 tabla 4
 inserts tabla 4
+
 -- Tabla e inserts de (persona 5)
 tabla 5
 inserts tabla 5
 
 -- Consultas de (persona 1)
-DELIMITER //
+DELIMITER $$
 
-CREATE PROCEDURE nombre_del_procedimiento (parametros)
+CREATE PROCEDURE consultas_nombrePersona1()
 BEGIN
-    -- Aquí va el código SQL
-END //
+    --Todas las consultas tanto individuales como grupales de persona1
+END$$
 
 DELIMITER ;
+
 -- Consultas de (persona 2)
-DELIMITER //
+DELIMITER $$
 
-CREATE PROCEDURE nombre_del_procedimiento (parametros)
+CREATE PROCEDURE consultas_nombrePersona2()
 BEGIN
-    -- Aquí va el código SQL
-END //
+    --Todas las consultas tanto individuales como grupales de persona2
+END$$
 
 DELIMITER ;
+
 -- Consultas de (persona 3)
-DELIMITER //
+DELIMITER $$
 
-CREATE PROCEDURE nombre_del_procedimiento (parametros)
+CREATE PROCEDURE consultas_nombrePersona3()
 BEGIN
-    -- Aquí va el código SQL
-END //
+    --Todas las consultas tanto individuales como grupales de persona3
+END$$
 
 DELIMITER ;
+
 -- Consultas de (persona 4)
-DELIMITER //
+DELIMITER $$
 
-CREATE PROCEDURE nombre_del_procedimiento (parametros)
+CREATE PROCEDURE consultas_nombrePersona4()
 BEGIN
-    -- Aquí va el código SQL
-END //
+    --Todas las consultas tanto individuales como grupales de persona4
+END$$
 
 DELIMITER ;
--- Consultas de (persona 5)
-DELIMITER //
 
-CREATE PROCEDURE nombre_del_procedimiento (parametros)
+-- Consultas de (persona 5)
+DELIMITER $$
+
+CREATE PROCEDURE consultas_nombrePersona5()
 BEGIN
-    -- Aquí va el código SQL
-END //
+    --Todas las consultas tanto individuales como grupales de persona5
+END$$
 
 DELIMITER ;
